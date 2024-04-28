705fd50d7d81a6c3c5a7e2f6b4ea404505982bb2
tevintchuinkam <tevintchuinkam@gmail.com>
2024-04-28 16:24:49 +0200

    define posix-like interface

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+type ChunckHandle struct {
+	Handle string
+}
+type ChunckServer struct {
+	Port string
+	ID   string
+}
+type Master struct {
+	FileToHandlesMap  map[string][]ChunckHandle
+	HandleToServerMap map[string]ChunckServer
+}
+
+type Orchastrator interface {
+	CreateFile(name string, dir string, data []byte) error
+	ReadFile(name string, dir string) ([]byte, error)
+	DeleteFile(name string, dir string) error
+	LS(dir string) error
+	CreateDir(name string) error
+	DeleteDir(name string) error
+}
+
+type Server interface {
+}
+
 func main() {
 
 }
