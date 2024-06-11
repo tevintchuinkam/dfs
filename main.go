4eb48be8ba7ab47ba3ab6f75381f323e37fe596d
tevintchuinkam <tevintchuinkam@gmail.com>
2024-06-11 11:29:53 +0200

    server type and methods

--- main.go
+++ main.go
@@ -1,94 +1,29 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io/fs"
 	"path"
 	"strings"
-	"sync"
-	"time"
 
 	"github.com/google/uuid"
 )
 
+const CHUNK_SIZE_BYTES = 1024
+
 // implements fs.FS
 var _ fs.FS = &TDFS{}
-
-type Chunck struct {
-	ID   string
-	Mu   sync.Mutex
-	data []byte
-}
 
 type ChunckServer struct {
 	Port string
 	ID   string
 }
 
-type File struct {
-	chunks []*Chunck
-}
-
-type FileInfo struct {
-}
-
-func (f *FileInfo) Name() string {
-	return "file"
-}
-func (f *FileInfo) Size() int64 {
-	return 0
-}
-func (f *FileInfo) Mode() fs.FileMode {
-	return 0
-}
-func (f *FileInfo) ModTime() time.Time {
-	return time.Now()
-}
-func (f *FileInfo) IsDir() bool {
-	return false
-}
-
-func (f *FileInfo) Sys() any {
-	return struct{}{}
-}
-
-// File implements fs.File
-func (f *File) Close() error {
-	return nil
-}
-func (f *File) Stat() (fs.FileInfo, error) {
-	return &FileInfo{}, nil
-}
-
-func (f *File) Read(b []byte) (int, error) {
-	return 0, nil
-}
-
-type directory struct {
-	name    string
-	prev    *directory
-	subDirs []*directory
-	files   map[string]*File
-}
-
-func (d *directory) String() string {
-	if d == nil {
-		return ""
-	}
-	var sb strings.Builder
-	sb.WriteString(d.name)
-	for fileName, _ := range d.files {
-		sb.WriteString(fmt.Sprintf("\n\t├── %s", fileName))
-	}
-	return sb.String()
-}
-
 type TDFS struct {
-	chunkSizeBytes int
-	currentDir     *directory
-	rootDir        *directory
-	chunkServers   map[string]ChunckServer
+	currentDir *directory
+	rootDir    *directory
+	server     Server
+	chunksize  int
 }
 
 // implements fs.FS
@@ -110,15 +45,20 @@ func (m *TDFS) CreateFile(name, dir string, data []byte) error {
 	processed := 0
 	for len(data) < processed {
 		chunk := new(Chunck)
-		chunk.ID = uuid.New().String()
-		for i := processed; i-processed < m.chunkSizeBytes; i++ {
+		chunk.ID = chunkID(uuid.New().String())
+		for i := processed; i-processed < m.ChunkSize(); i++ {
 			chunk.data = append(chunk.data, data[i])
 		}
 		file.chunks = append(file.chunks, chunk)
 		// TODO:  send the chunk to one of the file servers
-
 	}
 	return nil
+}
+
+// ChunkSize returns the chunk size used
+// in the file system
+func (n *TDFS) ChunkSize() int {
+	return n.chunksize
 }
 
 func (m *TDFS) ReadFile(name, dir string) ([]byte, error) {
@@ -148,7 +88,7 @@ func (d *directory) WalkTo(p string) (*directory, error) {
 			return dir.WalkTo(rest)
 		}
 	}
-	return nil, errors.New(fmt.Sprintf("directory does not exist: %s", base))
+	return nil, fmt.Errorf("directory does not exist: %s", base)
 }
 
 // creates a new TDFS filesystem with the given base directory
@@ -160,9 +100,11 @@ func New() *TDFS {
 		files:   make(map[string]*File),
 	}
 	return &TDFS{
-		chunkSizeBytes: 64,
-		currentDir:     root,
-		rootDir:        root,
-		chunkServers:   make(map[string]ChunckServer),
+		currentDir: root,
+		rootDir:    root,
+		server: Server{
+			chunkServers: make(map[chunkID]ChunckServer),
+		},
+		chunksize: 1024,
 	}
 }
