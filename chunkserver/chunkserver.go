66e8e4bf7f6b396bcb649015d7aa3f3dd8f02269
tevintchuinkam <tevintchuinkam@gmail.com>
2024-06-21 10:32:25 +0200

    grpc setup

--- chunkserver/chunkserver.go
+++ chunkserver/chunkserver.go
@@ -1,1 +1,27 @@
 package chunksever
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// accept connections
+	port := ":9000"
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen on port %s: %v", port, err)
+	}
+	s := grpc.NewServer()
+	// add routes
+	if err := s.Serve(lis); err != nil {
+
+	}
+
+	// store the incoming files
+
+	// return the requested files
+
+}
