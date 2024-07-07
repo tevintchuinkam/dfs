159379e5b2a5aa89e902406e07ebb4387b9136f1
tevintchuinkam <tevintchuinkam@gmail.com>
2024-07-07 17:46:29 +0200

    latency

--- main.go
+++ main.go
@@ -240,7 +240,7 @@ func gatherWorkStealingOptimisationData(iterations int) {
 
 	useCache := false
 	foldersPerLevel := 5
-	for latency := range 50 {
+	for latency := 0; latency < 30; latency += 2 {
 		slog.Info("restarting servers", "latency", latency)
 		stopAllServers()
 		startAllServers(time.Duration(latency) * time.Microsecond)
